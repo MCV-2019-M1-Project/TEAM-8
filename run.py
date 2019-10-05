ce77e96dc3a6c2e7526fd9a7aaf6f6cfd9c99012
EddieMG <eduardmgobierno@gmail.com>
2019-10-05 13:02:43 +0200

    Bug fixing and proper directories

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
 from dataset import HistDataset
 from distance import euclidean
 from utils import calc_similarities, get_tops
-
+import matplotlib.pyplot as plt
 QS1 = HistDataset("datasets/qsd1_w1")
 DB = HistDataset("datasets/DDBB")
 
 sims = calc_similarities(euclidean, DB, QS1, True)
 tops = get_tops(sims, 4)
 
-print(sims[0, tops[0]])
+# print(sims[0, tops[0]])
+# print(DB[87][1])
+R=DB[87][0]
+G=DB[87][1]
+B=DB[87][2]
+plt.plot(R,'r',G,'g',B,'b')
+plt.ylabel('Histogram')
+plt.show()
