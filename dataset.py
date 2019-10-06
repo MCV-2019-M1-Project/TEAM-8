1a667e9803092b35cece0105c2328ea78214f5a4
Alberto L <alberto1llv@gmail.com>
2019-10-06 03:35:19 +0200

    not al images loading

--- dataset.py
+++ dataset.py
@@ -5,44 +5,142 @@ import cv2
 
 
 class Dataset:
-    def __init__(self, path):
+    def __init__(self, path, mask):
         self.paths = glob.glob(f"{path}/*.jpg")
+        print(len(self.paths))
 
     def __getitem__(self, idx):
+        # return cv2.imread(self.paths[idx],cv2.IMREAD_GRAYSCALE)
         return cv2.imread(self.paths[idx])
-
     def __len__(self):
         return len(self.paths)
 
 
 class HistDataset(Dataset):
+    """
+        Calculates the histogram of the images
+        and applies a mask on from RGB to HLS on the Histogram calculation
+    """
+
+
     def __init__(self, *args, caching=True, **kwargs):
         self.caching = caching
         if caching:
             self.cache = dict()
         super().__init__(*args, **kwargs)
+        print(args)
+        self.masking = args[1]
 
-    @staticmethod
-    def calc_hist(img):
+    def calc_hist(self, img):
         def calc_mask():
-            result = np.zeros(img.shape[:2], dtype="uint8")
 
-            hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
-            hls_split = cv2.split(hls)
+            if False:
+                result = np.zeros(img.shape[:2], dtype="uint8")
 
-            lu = np.asarray(hls_split[1])
-            sa = np.asarray(hls_split[2])
+                hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+                hls_split = cv2.split(hls)
 
-            sat_thresh = 100
-            lum_tresh = 75
+                lu = np.asarray(hls_split[1])
+                sa = np.asarray(hls_split[2])
 
-            result[(sa > sat_thresh) & (lu < lum_tresh)] = 1
-            return result
+                sat_thresh = 100
+                lum_tresh = 75
+
+                result[(sa > sat_thresh) & (lu < lum_tresh)] = 1
+
+                #return result
+
+                pts = cv2.findNonZero(result).squeeze().tolist()
+
+                num = 50
+
+                pts_up_left = np.asarray(sorted(pts, key=lambda x: (x[0] + x[1]))[1:num])
+
+                pts_bottom_left = np.asarray(sorted(pts, key=lambda x: (-(x[1] / (x[0] + 1))))[1:num])
+
+                pts_up_right = np.asarray(sorted(pts, key=lambda x: (-(x[0] / (x[1] + 1))))[1:num])
+
+                pts_bottom_right = np.asarray(sorted(pts, key=lambda x: (-x[0] + -x[1]))[1:num])
+
+                a = 9999
+                pt_up_left = pts_up_left[0]
+                pt_up_right = pts_up_right[0]
+
+                for y in range(1000):
+                    for x in range(1000):
+                        a = abs(pts_up_left[y][1] - pts_up_right[x][1])
+                        if a < 75:
+                            pt_up_left = pts_up_left[y]
+                            pt_up_right = pts_up_right[x]
+                            break
+                    if a < 75:
+                        break
+
+                diff = np.abs(pts_bottom_right[:][0] - pt_up_right[0])
+                pt_bottom_right = pts_bottom_right[np.where(diff == np.amin(diff))[0]][0]
+
+                diff = np.abs(pts_bottom_left[:][1] - pt_bottom_right[1])
+                pt_bottom_left = pts_bottom_left[np.where(diff == np.amin(diff))[0]][0]
+
+                points = np.array((pt_up_right, pt_up_left, pt_bottom_left, pt_bottom_right))
+                res2 = cv2.fillPoly(result, np.int32([points]), 1, 8, 0, None)
+
+                """im = cv2.circle(img, (pt_up_left[0], pt_up_left[1]), 10, (255, 0, 0), 3) #blue
+                im = cv2.circle(img, (pt_up_right[0], pt_up_right[1]), 10, (0, 255, 0), 3) #green
+                im = cv2.circle(img, (pt_bottom_right[0], pt_bottom_right[1]), 10, (255, 255, 255), 3) #white
+                im = cv2.circle(img, (pt_bottom_left[0], pt_bottom_left[1]), 10, (0, 0, 255), 3) #red
+
+                scale_percent = 25  # percent of original size
+                width = int(img.shape[1] * scale_percent / 100)
+                height = int(img.shape[0] * scale_percent / 100)
+                dim = (width, height)
+
+                resized1 = cv2.resize(res2, dim, interpolation=cv2.INTER_AREA)
+                resized2 = cv2.resize(im, dim, interpolation=cv2.INTER_AREA)
+
+                cv2.imshow("a", resized1)
+                cv2.imshow("img",resized2)
+                cv2.waitKey()
+                cv2.destroyAllWindows()"""
+
+                return res2
+
+            else :
+                im = cv2.GaussianBlur(img, (5, 5), 0)
+
+                gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+                ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+                i, j = np.where(thresh == 255)
+                y1 = i[0]  # fist point
+                x1 = j[0]
+                y4 = i[-1]  # last point
+                x4 = j[-1]
+
+                k, d = np.where(thresh[:, 0:100] == 255)
+                y2 = k[0]  # second point
+                x2 = d[0]
+                y3 = k[-1]  # third point
+                x3 = d[-1]
+
+                points = np.array([[11, 13],
+                                   [14, 16],
+                                   [17, 11],
+                                   [12, 15]]).astype('int32')
+
+                points[1] = (j[0], i[0])
+                points[2] = (d[0], k[0])
+                points[3] = (d[-1], k[-1])
+                points[0] = (j[-1], i[-1])
+
+                image_countours = cv2.fillPoly(thresh, np.int32([points]), (255, 255, 255), 8, 0, None)
+
+                return image_countours
 
         """
         if background should be removed or not
         """
-        if True:
+        if self.masking:
             mask = calc_mask()
         else:
             mask = None
--- run.py
+++ run.py
@@ -1,18 +1,21 @@
 import ml_metrics as metrics
+from dataset import Dataset
 from dataset import HistDataset
 import distance as dist
 from utils import calc_similarities, get_tops, get_groundtruth, normalize_hist
-
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
 '''For background removal vis HLS values go to dataset.py and check True, didn't have time to put it here cleanly'''
 
 groundTruth = get_groundtruth("datasets/qsd2_w1/gt_corresps.pkl")
-
-QS = [normalize_hist(qs_hist) for qs_hist in HistDataset("datasets/qsd2_w1")]
-DB = [normalize_hist(db_hist) for db_hist in HistDataset("datasets/DDBB")]
+#QS = [normalize_hist(qs_hist) for qs_hist in HistDataset("datasets/qsd1_w1")]
+QS2 = [normalize_hist(qs_hist) for qs_hist in HistDataset("datasets/qsd2_w1", True)]
+DB = [normalize_hist(db_hist) for db_hist in HistDataset("datasets/DDBB", False)]
 
 k = 10
 
-sims = calc_similarities(dist.canberra, DB, QS, True)
+sims = calc_similarities(dist.canberra, DB, QS2, True)
 tops = get_tops(sims, k)
 mapAtK = metrics.mapk(groundTruth, tops, k)
 
@@ -21,3 +24,61 @@ print(str(tops[1]))
 print(str(tops[2]))
 
 print("Map@k is " + str(mapAtK))
+
+#If you want to display any specific histogram
+# R=DB[87][0]
+# G=DB[87][1]
+# B=DB[87][2]
+# plt.plot(R,'r',G,'g',B,'b')
+# plt.ylabel('Histogram')
+# plt.show()
+
+#for imgi in QS2:
+
+"""
+    scale_percent = 25 # percent of original size
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    # resize image
+    
+    resized1 = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    resized2 = cv2.resize(image_countours, dim, interpolation = cv2.INTER_AREA)
+
+    cv2.imshow("dd", resized1)
+    cv2.imshow("dad", resized2)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()"""
+
+# print("dd")
+# print(k)
+# plt.imshow(thresh)
+# plt.show()
+# cv2.waitKey(0)
+
+
+# print(QS2[1][:,:,1])
+# imgplot=plt.imshow(QS2[1])
+# cv2.imshow('red',QS2[1][:,:,0])
+# cv2.imshow('green',QS2[1][:,:,1])
+# cv2.imshow('blue',QS2[1][:,:,2])
+# imgray = cv2.cvtColor(QS2[1], cv2.COLOR_BGR2GRAY)
+# ret, thresh = cv2.threshold(imgray, 100, 255, 0)
+# countours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+# image_countours=cv2.drawContours(QS2[1],countours,-1, (0,255,0), 3)
+# image_countours=cv2.resize(image_countours,(1920,1080))
+# cv2.imshow('countures',image_countours)
+# print(countours)
+# imgplot=plt.imshow(cv2.cvtColor(QS2[1], cv2.COLOR_BGR2RGB))
+# plt.show()
+# cv2.waitKey(0)
+
+
+
+# edgeImg = np.max(np.array([edgedetector(denoised_dataset[1][:, :, 0]), edgedetector(denoised_dataset[1][:, :, 1]), edgedetector(denoised_dataset[1][:, :, 2])]),axis=0)
+# cv2.imshow('countures', edgeImg)
+
+
+
+
