5c588bc896dfca6e7439e86e2ffd28eebd099376
Alberto L <alberto1llv@gmail.com>
2019-10-06 13:23:52 +0200

    Deleting some garbage

--- dataset.py
+++ dataset.py
@@ -33,78 +33,6 @@ class HistDataset(Dataset):
 
     def calc_hist(self, img):
         def calc_mask():
-            if False:
-                result = np.zeros(img.shape[:2], dtype="uint8")
-
-                hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
-                hls_split = cv2.split(hls)
-
-                lu = np.asarray(hls_split[1])
-                sa = np.asarray(hls_split[2])
-
-                sat_thresh = 100
-                lum_tresh = 75
-
-                result[(sa > sat_thresh) & (lu < lum_tresh)] = 1
-
-                #return result
-
-                pts = cv2.findNonZero(result).squeeze().tolist()
-
-                num = 50
-
-                pts_up_left = np.asarray(sorted(pts, key=lambda x: (x[0] + x[1]))[1:num])
-
-                pts_bottom_left = np.asarray(sorted(pts, key=lambda x: (-(x[1] / (x[0] + 1))))[1:num])
-
-                pts_up_right = np.asarray(sorted(pts, key=lambda x: (-(x[0] / (x[1] + 1))))[1:num])
-
-                pts_bottom_right = np.asarray(sorted(pts, key=lambda x: (-x[0] + -x[1]))[1:num])
-
-                a = 9999
-                pt_up_left = pts_up_left[0]
-                pt_up_right = pts_up_right[0]
-
-                for y in range(1000):
-                    for x in range(1000):
-                        a = abs(pts_up_left[y][1] - pts_up_right[x][1])
-                        if a < 75:
-                            pt_up_left = pts_up_left[y]
-                            pt_up_right = pts_up_right[x]
-                            break
-                    if a < 75:
-                        break
-
-                diff = np.abs(pts_bottom_right[:][0] - pt_up_right[0])
-                pt_bottom_right = pts_bottom_right[np.where(diff == np.amin(diff))[0]][0]
-
-                diff = np.abs(pts_bottom_left[:][1] - pt_bottom_right[1])
-                pt_bottom_left = pts_bottom_left[np.where(diff == np.amin(diff))[0]][0]
-
-                points = np.array((pt_up_right, pt_up_left, pt_bottom_left, pt_bottom_right))
-                res2 = cv2.fillPoly(result, np.int32([points]), 1, 8, 0, None)
-
-                """im = cv2.circle(img, (pt_up_left[0], pt_up_left[1]), 10, (255, 0, 0), 3) #blue
-                im = cv2.circle(img, (pt_up_right[0], pt_up_right[1]), 10, (0, 255, 0), 3) #green
-                im = cv2.circle(img, (pt_bottom_right[0], pt_bottom_right[1]), 10, (255, 255, 255), 3) #white
-                im = cv2.circle(img, (pt_bottom_left[0], pt_bottom_left[1]), 10, (0, 0, 255), 3) #red
-
-                scale_percent = 25  # percent of original size
-                width = int(img.shape[1] * scale_percent / 100)
-                height = int(img.shape[0] * scale_percent / 100)
-                dim = (width, height)
-
-                resized1 = cv2.resize(res2, dim, interpolation=cv2.INTER_AREA)
-                resized2 = cv2.resize(im, dim, interpolation=cv2.INTER_AREA)
-
-                cv2.imshow("a", resized1)
-                cv2.imshow("img",resized2)
-                cv2.waitKey()
-                cv2.destroyAllWindows()"""
-
-                return res2
-
-            else:
                 im = cv2.GaussianBlur(img, (5, 5), 0)
 
                 gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
@@ -122,25 +50,8 @@ class HistDataset(Dataset):
                 # Finding unknown region
                 sure_fg = np.uint8(sure_fg)
                 unknown = cv2.subtract(sure_bg, sure_fg)
-
-                #scale_percent = 35  # percent of original size
-                #width = int(img.shape[1] * scale_percent / 100)
-                #height = int(img.shape[0] * scale_percent / 100)
-                #dim = (width, height)
-
-                #resized1 = cv2.resize(sure_fg, dim, interpolation=cv2.INTER_AREA)
-                #resized2 = cv2.resize(unknown, dim, interpolation=cv2.INTER_AREA)
-
-                #cv2.imshow("a", resized1)
-                #cv2.imshow("img", resized2)
-                #cv2.waitKey()
-                #cv2.destroyAllWindows()
-
-                #cv2.waitKey()
-
                 i, j = np.where(unknown == 255)
                 k, d = np.where(thresh[:, 0:100] == 255)
-
 
                 points = np.array([[11, 13],
                                    [14, 16],
