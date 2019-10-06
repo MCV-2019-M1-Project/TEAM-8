04cfeb378166c8c6f77319661d9caf58b8d78533
Alberto L <alberto1llv@gmail.com>
2019-10-06 16:13:47 +0200

    run.py cleaned and included both scenarios

--- run.py
+++ run.py
@@ -11,38 +11,46 @@ from utils import (
     get_mask_metrics,
 )
 
-"""For background removal vis HLS values go to dataset.py and check True, didn't have time to put it here cleanly"""
 
-groundTruth = get_groundtruth("datasets/qsd2_w1/gt_corresps.pkl")
-QS = [normalize_hist(qs_hist) for qs_hist in HistDataset("datasets/qsd1_w1")]
-QS2 = [
-    normalize_hist(qs_hist) for qs_hist in HistDataset("datasets/qsd2_w1", masking=True)
-]
-DB = [
-    normalize_hist(db_hist) for db_hist in HistDataset("datasets/DDBB", masking=False)
-]
+def find_img_corresp(QS, groundTruth, masking):
+    sims = calc_similarities(dist.canberra, DB, QS, True)
+    tops = get_tops(sims, k)
+    mapAtK = metrics.mapk(groundTruth, tops, k)
+
+    print(str(tops[0]))
+    print(str(tops[1]))
+    print(str(tops[2]))
+    print("Map@k is " + str(mapAtK))
+
+    if masking:
+        gt_masks = MaskDataset("datasets/qsd2_w1")
+        mask_dataset = HistDataset("datasets/qsd2_w1", masking=True)
+        predicted_masks = [cv2.threshold(mask_dataset.get_mask(i), 128, 1, cv2.THRESH_BINARY)[1] for i, item in enumerate(mask_dataset)]
+
+        mask_metrics = get_mask_metrics(predicted_masks, gt_masks, True)
+
+        print("Precision: " + str(mask_metrics["precision"]))
+        print("Recall: " + str(mask_metrics["recall"]))
+        print("F1-score: " + str(mask_metrics["f1_score"]))
 
 
-mask_dataset = HistDataset("datasets/qsd2_w1", masking=True)
+groundTruth1 = get_groundtruth("datasets/qsd1_w1/gt_corresps.pkl")
+groundTruth2 = get_groundtruth("datasets/qsd2_w1/gt_corresps.pkl")
 
-predicted_masks = [cv2.threshold(mask_dataset.get_mask(i), 128, 1, cv2.THRESH_BINARY)[1] for i, item in enumerate(mask_dataset)]
-gt_masks = MaskDataset("datasets/qsd2_w1")
+QS1 = [normalize_hist(qs_hist) for qs_hist in HistDataset("datasets/qsd1_w1")]
+QS2 = [normalize_hist(qs_hist) for qs_hist in HistDataset("datasets/qsd2_w1", masking=True)]
+DB = [normalize_hist(db_hist) for db_hist in HistDataset("datasets/DDBB", masking=False)]
 
 k = 10
 
-sims = calc_similarities(dist.canberra, DB, QS2, True)
-tops = get_tops(sims, k)
-mapAtK = metrics.mapk(groundTruth, tops, k)
+print("Analyzing QS1")
+find_img_corresp(QS1, groundTruth1, False)
 
-print(str(tops[0]))
-print(str(tops[1]))
-print(str(tops[2]))
+print("Analyzing QS2")
+find_img_corresp(QS2, groundTruth2, True)
 
-print("Map@k is " + str(mapAtK))
 
-mask_metrics = get_mask_metrics(predicted_masks, gt_masks, True)
 
-print("Precision: " + str(mask_metrics["precision"]))
-print("Recall: " + str(mask_metrics["recall"]))
-print("F1-score: " + str(mask_metrics["f1_score"]))
+
+
 
