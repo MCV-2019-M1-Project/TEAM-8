18654e4302aa400b0320ac283c5a56c4d88dd5aa
EddieMG <eduardmgobierno@gmail.com>
2019-10-13 16:05:25 +0200

    Algorithm beta phase

--- run.py
+++ run.py
@@ -3,51 +3,57 @@ import cv2
 from dataset import HistDataset
 from dataset import MaskDataset
 import distance as dist
-from utils import (
-    calc_similarities,
-    get_tops,
-    get_groundtruth,
-    normalize_hist,
-    get_mask_metrics,
-)
+import text_removal
+#from utils import (
+#    calc_similarities,
+#    get_tops,
+#    get_groundtruth,
+#    normalize_hist,
+#    get_mask_metrics,
+#)
+
+namedataset= [print('2') for im in text_removal.text_remover("qsd1_w2")]
+cv2.imshow('try', namedataset[1])
+cv2.waitKey(0)
+print('here')
+
+cv2.waitkey(0)
+#def find_img_corresp(QS, groundTruth, masking):
+#    sims = calc_similarities(dist.canberra, DB, QS, True)
+#    tops = get_tops(sims, k)
+#    mapAtK = metrics.mapk(groundTruth, tops, k)
+
+#    print(str(tops[0]))
+#    print(str(tops[1]))
+#    print(str(tops[2]))
+#    print("Map@k is " + str(mapAtK))
+
+#    if masking:
+#        gt_masks = MaskDataset("datasets/qsd2_w1")
+#        mask_dataset = HistDataset("datasets/qsd2_w1", masking=True)
+#        predicted_masks = [cv2.threshold(mask_dataset.get_mask(i), 128, 1, cv2.THRESH_BINARY)[1] for i, item in enumerate(mask_dataset)]
+
+#        mask_metrics = get_mask_metrics(predicted_masks, gt_masks, True)
+
+#        print("Precision: " + str(mask_metrics["precision"]))
+#        print("Recall: " + str(mask_metrics["recall"]))
+#        print("F1-score: " + str(mask_metrics["f1_score"]))
 
 
-def find_img_corresp(QS, groundTruth, masking):
-    sims = calc_similarities(dist.canberra, DB, QS, True)
-    tops = get_tops(sims, k)
-    mapAtK = metrics.mapk(groundTruth, tops, k)
+#groundTruth1 = get_groundtruth("datasets/qsd1_w1/gt_corresps.pkl")
+#groundTruth2 = get_groundtruth("datasets/qsd2_w1/gt_corresps.pkl")
 
-    print(str(tops[0]))
-    print(str(tops[1]))
-    print(str(tops[2]))
-    print("Map@k is " + str(mapAtK))
+#QS1 = [normalize_hist(qs_hist) for qs_hist in HistDataset("datasets/qsd1_w1")]
+#QS2 = [normalize_hist(qs_hist) for qs_hist in HistDataset("datasets/qsd2_w1", masking=True)]
+#DB = [normalize_hist(db_hist) for db_hist in HistDataset("datasets/DDBB", masking=False)]
 
-    if masking:
-        gt_masks = MaskDataset("datasets/qsd2_w1")
-        mask_dataset = HistDataset("datasets/qsd2_w1", masking=True)
-        predicted_masks = [cv2.threshold(mask_dataset.get_mask(i), 128, 1, cv2.THRESH_BINARY)[1] for i, item in enumerate(mask_dataset)]
+#k = 10
 
-        mask_metrics = get_mask_metrics(predicted_masks, gt_masks, True)
+#print("Analyzing QS1")
+#find_img_corresp(QS1, groundTruth1, False)
 
-        print("Precision: " + str(mask_metrics["precision"]))
-        print("Recall: " + str(mask_metrics["recall"]))
-        print("F1-score: " + str(mask_metrics["f1_score"]))
-
-
-groundTruth1 = get_groundtruth("datasets/qsd1_w1/gt_corresps.pkl")
-groundTruth2 = get_groundtruth("datasets/qsd2_w1/gt_corresps.pkl")
-
-QS1 = [normalize_hist(qs_hist) for qs_hist in HistDataset("datasets/qsd1_w1")]
-QS2 = [normalize_hist(qs_hist) for qs_hist in HistDataset("datasets/qsd2_w1", masking=True)]
-DB = [normalize_hist(db_hist) for db_hist in HistDataset("datasets/DDBB", masking=False)]
-
-k = 10
-
-print("Analyzing QS1")
-find_img_corresp(QS1, groundTruth1, False)
-
-print("Analyzing QS2")
-find_img_corresp(QS2, groundTruth2, True)
+#print("Analyzing QS2")
+#find_img_corresp(QS2, groundTruth2, True)
 
 
 
--- text_removal.py
+++ text_removal.py
@@ -1,6 +1,36 @@
-class test_remover(dataset):
+import dataset
+import glob
+import numpy as np
+import cv2
+class text_remover(dataset.Dataset):
     
+    def __init__(self, path):
+        self.paths = sorted(glob.glob(f"{path}/*.jpg"))
+        
+    def getpoints(self, img):
+
+        im = cv2.GaussianBlur(img, (5, 5), 0)
+
+        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        ret, thresh = cv2.threshold(
+            gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        kernel = np.ones((3, 3), np.uint8)
+        sure_bg = cv2.dilate(thresh, kernel, iterations=3)
+        print('here')
+        #cv2.imshow('try',sure_bg[1])
+        #cv2.waitKey(0)
+        
+        #cv2.waitkey(0)
+        return sure_bg
 
 
+
+
+
+
+
+    def __getitem__(self, idx):
+        print('1')
+        return self.getpoints(super().__getitem__(idx))
 
 
