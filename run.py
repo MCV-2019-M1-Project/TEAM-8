9045ada76c7e81941474b76732691f15a11ed654
EddieMG <eduardmgobierno@gmail.com>
2019-10-19 00:26:36 +0200

    Several Methods added, accuracy improved

--- run.py
+++ run.py
@@ -12,41 +12,51 @@ import text_removal
 #    normalize_hist,
 #    get_mask_metrics,
 #)
-#namedataset=[text_removal.getpoints(im) for im in text_removal.text_remover("datasets/qsd1_w2")]
-
-    
-##Fer el threshold per colors, ja que el valor de igual nomes el tenen el retols.
-#image_countours=cv2.resize(namedataset[1],(1280,
+# namedataset=[text_removal.getpoints2(im) for im in text_removal.text_remover("datasets/qsd1_w2")]
+#
+# #
+# # ##Fer el threshold per colors, ja que el valor de igual nomes el tenen el retols.
+# image_countours=cv2.resize(namedataset[1],(1280,
 #                                           700))
-#cv2.imshow('try', image_countours)
-#cv2.waitKey(0)
+# cv2.imshow('try', image_countours)
+# cv2.waitKey(0)
+# #
 
-
-namedataset=[text_removal.getpoints(im) for im in text_removal.text_remover("datasets/qsd1_w2")]
+namedataset=[text_removal.getpoints2(im) for im in text_removal.text_remover("datasets/qsd1_w2")]
 print(len(namedataset))
-for  im in range(len(namedataset)):
+for im in range(len(namedataset)):
     print(im)
-    cv2.imwrite("C:/Users/Eduard/source/repos/MCV-2019-M1-Project/TEAM-8/"+str(im)+".png",namedataset[im])
+    cv2.imwrite("outputs/"+str(im)+".png",namedataset[im])
+
+# histogramas=[text_removal.getpoints4(im)for im in text_removal.text_remover("datasets/qsd1_w2")]
+# print(type(histogramas[0][0]))
+# plt.plot(histogramas[0])
+# plt.show()
+# cv2.imshow('try', histogramas[0])
+# cv2.waitKey(0)
+# print(histogramas[1])
 ##Fer el threshold per colors, ja que el valor de igual nomes el tenen el retols.
 #image_countours=cv2.resize(namedataset[1],(1280,
                                            #700))
-#cv2.imshow('try', image_countours)
-#cv2.waitKey(0)
 
 
 
 
 
 
-#dataset1=[text_removal.getimg(im) for im in text_removal.text_remover("datasets/qsd1_w2")]
+
+# dataset1=[text_removal.getimg(im) for im in text_removal.text_remover("datasets/qsd1_w2")]
 
 #hsv = cv2.cvtColor(dataset1[1], cv2.COLOR_BGR2HSV)
-##hist=cv2.calcHist(dataset1[1], [0], None, [256], [0, 256])
-#image_countours=cv2.resize(hsv,(1280, 700))
+# hist=cv2.calcHist(dataset1[0], [0], None, [256], [0, 256])
+# image_countours=cv2.resize(hsv,(1280, 700))
 #cv2.imshow('try', image_countours)
-#cv2.waitKey(0)
-#plt.plot(hist[0])
-#plt.show()
+# cv2.waitKey(0)
+# print(type(dataset1[0][:,:,0]))
+# cv2.imshow('try', dataset1[0][:,:,0])
+# cv2.waitKey(0)
+# plt.plot(dataset1[0][:,:,0])
+# plt.show()
 
 
 
--- text_removal.py
+++ text_removal.py
@@ -50,31 +50,91 @@ def getpoints(im):
     #return hsv
 
 def getpoints2(im):
-    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(
-    gray, 200, 255, cv2.THRESH_BINARY)
-    edges=cv2.Canny(im,200,220)
-    kerneledges = np.array([[0, 1,1,1,1,1,1,1,1,1],
-                            [0, 1,1,1,1,1,1,1,1,1],
-                            [0, 1,1,1,1,1,1,1,1,1],
-                            [0, 1,1,1,1,1,1,1,1,1],
-                            [0, 1,1,1,1,1,1,1,1,1],
-                            [0, 1,1,1,1,1,1,1,1,1],
-                            [0, 1,1,1,1,1,1,1,1,1],
-                            [0, 1,1,1,1,1,1,1,1,1],
-                            [0, 1,1,1,1,1,1,1,1,1],
-                            [0, 1,1,1,1,1,1,1,1,1],
-                            [0, 1,1,1,1,1,1,1,1,1],
-                            [0, 1,1,1,1,1,1,1,1,1],
-                            [0, 1,1,1,1,1,1,1,1,1]], np.uint8)
-    kernel = np.ones((20), np.uint8)
+    # print(im.shape[0])
+    imager=im[:,:,0]
+    imageg=im[:,:,1]
+    imageb=im[:,:,2]
+    output=np.equal(imager,imageg,dtype=int)
+    output2=np.equal(imager,imageb,dtype=int)
+    output1=np.equal(imageb,imageg,dtype=int)
+    output3=output&output2
+    output4=output3&output1
+    mask=255*output4
+    mask1=mask.astype(np.uint8)
+    # gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    # ret, thresh = cv2.threshold(
+    # gray, 200, 255, cv2.THRESH_BINARY)
+    kernel = np.ones((int((im.shape[0])/350),int((im.shape[1])/50)), np.uint8)
     #edge = cv2.erode(thresh, kerneledges, iterations=3)
-    denoised = cv2.erode(thresh, kernel, iterations=3)
-    print(type(im))
-
+    denoised = cv2.erode(mask1, kernel, iterations=3)
+    denoised = cv2.erode(denoised, kernel, iterations=3)
+    denoised = cv2.dilate(denoised, kernel, iterations=3)
+    denoised = cv2.dilate(denoised, kernel, iterations=3)
     return denoised
-  
-
-    
 
 
+def getpoints3(im):
+    histr=cv2.calcHist([im], [0], None, [256], [0, 256]),
+    histg=cv2.calcHist([im], [1], None, [256], [0, 256]),
+    histb=cv2.calcHist([im], [2], None, [256], [0, 256]),
+    histrg=[]*256
+    histrb=[]*256
+    histresultat=[]*256
+    print(histr[0][0])
+    print(histb[0][0])
+    print(histg[0][0])
+    histrg=np.equal(histr,histg)
+    histrb=np.equal(histr,histb)
+    histresultat=histrb&histrg
+    # for i in range(len(histg)):
+    #     if int(histr[0][i]) == int(histg[0][i]):
+    #         histrg[0][i] = histr[0][i]
+    #     else:
+    #         histrg[0][i] = 0
+    #     if int(histr[0][i]) == int(histb[0][i]):
+    #         histrb[0][i] = histr[0][i]
+    #     else:
+    #         histrb[0][i] = 0
+    #
+    #     if int(histrg[0][i]) == int(histrb[0][i]):
+    #         histresultat[0][i] = int(histrb[0][i])
+    #     else:
+    #         histresultat[0][i]==0
+        # histrg[i]=histr[i]&histg[i]
+        # histrb[i]=histr[i]&histb[i]
+        # histresultat[i]=histrb[i]&histrg[i]
+    # for i in range(len(histg)):
+    #     if histr[0].index(i) == histg[0].index(i):
+    #         histrg[0][i] = histr[0][i]
+    #     else:
+    #         histrg[0][i] = 0
+    #     if int(histr[0][i]) == int(histb[0][i]):
+    #         histrb[0][i] = histr[0][i]
+    #     else:
+    #         histrb[0][i] = 0
+    #
+    #     if int(histrg[0][i]) == int(histrb[0][i]):
+    #         histresultat[0][i] = int(histrb[0][i])
+    #     else:
+    #         histresultat[0][i] == 0
+
+    return histg
+def getpoints4(im):
+    imager=im[:,:,0]
+    imageg=im[:,:,1]
+    imageb=im[:,:,2]
+    output=np.equal(imager,imageg,dtype=int)
+    output2=np.equal(imager,imageb,dtype=int)
+    output1=np.equal(imageb,imageg,dtype=int)
+    output3=output&output2
+    output4=output3&output1
+
+    return output4
+
+
+
+
+
+
+def getimg(im):
+    return im
