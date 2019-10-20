eb1f7757f82827a2fb5f9f5ab725874f7583f87c
Alberto L <alberto1llv@gmail.com>
2019-10-20 14:31:56 +0200

    adapting kernel sizes

--- text_removal.py
+++ text_removal.py
@@ -2,6 +2,7 @@ import dataset
 import glob
 import numpy as np
 import cv2
+import utils
 class text_remover(dataset.Dataset):
     
     def __init__(self, path):
@@ -82,7 +83,140 @@ def getpoints2(im):
             self.boundingxy = boundingxy
             self.drawing = drawing
 
-    boundingxy=[boundRect[-1][0],boundRect[-1][1],boundRect[-1][0] + boundRect[-1][2], boundRect[-1][1] + boundRect[-1][3]]
+    boundingxy = [boundRect[-1][0], boundRect[-1][1], boundRect[-1][0] + boundRect[-1][2], boundRect[-1][1] + boundRect[-1][3]]
+
+    scale = 1
+    delta = 0
+    ddepth = cv2.CV_16S
+
+    mod = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(13,13))
+
+    # Otsu's thresholding
+    imx = np.copy(gray[boundingxy[1]:boundingxy[3], boundingxy[0]:boundingxy[2]])
+    ima, th2 = cv2.threshold(imx, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    minv = np.min(th2)
+    maxv = np.max(th2)
+    meanv = np.mean(th2)
+
+    diffmin = abs(meanv - minv)
+    diffmax = abs(meanv - maxv)
+
+    background = maxv
+    frground = minv
+
+    if diffmin < diffmax:
+        background = minv
+        frground = maxv
+
+    maxwidth = 0
+    n = 0
+
+    for i in range(imx.shape[0]):
+        width = 0
+        for j in range(imx.shape[1]):
+            if th2[i, j] == frground:
+                width += 1
+            elif width > 0:
+                maxwidth += width
+                n += 1
+                width = 0
+
+    maxwidth = round(maxwidth / n * 1.5)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (maxwidth, maxwidth))
+
+    if diffmin < diffmax:
+        print("maxwidth open: ", maxwidth)
+        mod = cv2.morphologyEx(mod, cv2.MORPH_OPEN, kernel)
+    else:
+        print("maxwidth closen: ", maxwidth)
+        mod = cv2.morphologyEx(mod, cv2.MORPH_CLOSE, kernel)
+
+
+    #print("Background: ", background / 255)
+    #print("Foreground: ", frground / 255)
+
+    grad_x = cv2.Sobel(mod, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+    # Gradient-Y
+    # grad_y = cv.Scharr(gray,ddepth,0,1)
+    grad_y = cv2.Sobel(mod, ddepth, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+
+    abs_grad_x = cv2.convertScaleAbs(grad_x)
+    abs_grad_y = cv2.convertScaleAbs(grad_y)
+
+    grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+    thresh = 30
+
+    grad[boundingxy[1]:boundingxy[3], boundingxy[0]:boundingxy[2]] = 0
+
+    diff2 = False
+    for i in range(boundingxy[3], min(boundingxy[3] + 50, grad.shape[0])):
+        broken = False
+        for j in range(boundingxy[0], boundingxy[2]):
+            if grad[i, j] > thresh:
+                broken = True
+                if diff2:
+                    boundingxy[3] -= 2
+                break
+        if broken:
+            break
+        else:
+            boundingxy[3] += 1
+            diff2 = True
+
+    diff1 = False
+    for i in range(boundingxy[1], boundingxy[1] - 50, -1):
+        broken = False
+        for j in range(boundingxy[0], boundingxy[2]):
+            if grad[i, j] > thresh:
+                broken = True
+                if diff1:
+                    boundingxy[1] += 2
+                break
+        if broken:
+            break
+        else:
+            boundingxy[1] -= 1
+            diff1 = True
+
+    for j in range(boundingxy[0], boundingxy[0] - 50, -1):
+        broken = False
+        for i in range(boundingxy[1], boundingxy[3]):
+            if grad[i, j] > thresh:
+                broken = True
+                break
+        if broken:
+            break
+        else:
+            boundingxy[0] -= 1
+
+    for j in range(boundingxy[2], boundingxy[2] + 50):
+        broken = False
+        for i in range(boundingxy[1], boundingxy[3]):
+            if grad[i, j] > thresh:
+                broken = True
+                break
+        if broken:
+            break
+        else:
+            boundingxy[2] += 1
+
+    drawing = cv2.rectangle(drawing, (boundingxy[0], boundingxy[1]), (boundingxy[2], boundingxy[3]), (0, 255, 255), 2)
+
+    if diff1:
+        boundingxy[1] -= 2
+
+    if diff2:
+        boundingxy[3] += 2
+
+    if False:
+        cv2.imshow("b", utils.resize(im, 50))
+        cv2.imshow("c", utils.resize(drawing, 50))
+        cv2.waitKey(0)
+
     #Coordinates [tlx,tly,brx,bry]
 
     return Result(boundingxy, drawing)
--- utils.py
+++ utils.py
@@ -5,7 +5,6 @@ import cv2
 from mask_metrics import MaskMetrics
 import distance as dist
 
-
 def calc_similarities(measure, db, qs, show_progress=False):
     """
     Returns an array of size (qs_size x db_size)
@@ -89,3 +88,57 @@ def lapl_at_index(source, index):
            - (1 * source[i, j+1]) \
            - (1 * source[i, j-1])
     return val
+
+
+def convolve(image, kernel):
+    # grab the spatial dimensions of the image, along with
+    # the spatial dimensions of the kernel
+    (iH, iW) = image.shape[:2]
+    (kH, kW) = kernel.shape[:2]
+
+    # allocate memory for the output image, taking care to
+    # "pad" the borders of the input image so the spatial
+    # size (i.e., width and height) are not reduced
+    pad = (kW - 1) // 2
+    image = cv2.copyMakeBorder(image, pad, pad, pad, pad,
+                               cv2.BORDER_REPLICATE)
+    output = np.zeros((iH, iW), dtype="float32")
+
+    # loop over the input image, "sliding" the kernel across
+    # each (x, y)-coordinate from left-to-right and top to
+    # bottom
+    for y in np.arange(pad, iH + pad):
+        for x in np.arange(pad, iW + pad):
+            # extract the ROI of the image by extracting the
+            # *center* region of the current (x, y)-coordinates
+            # dimensions
+            roi = image[y - pad:y + pad + 1, x - pad:x + pad + 1]
+
+            # perform the actual convolution by taking the
+            # element-wise multiplicate between the ROI and
+            # the kernel, then summing the matrix
+            k = (roi * kernel).sum()
+
+            # store the convolved value in the output (x,y)-
+            # coordinate of the output image
+            output[y - pad, x - pad] = k
+
+    # rescale the output image to be in the range [0, 255]
+    #output = rescale_intensity(output, in_range=(0, 255))
+    output = (output * 255).astype("uint8")
+
+    # return the output image
+    return output
+
+
+def resize(img, percent):
+    scale_percent = percent  # percent of original size
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+
+    return cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+
+
+def round_up_to_uneven(f):
+    return round((f - 1) / 2.) * 2 + 1
