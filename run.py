4db1f95c8116da2d64d7f00fc7d0f0a71fb6bf88
Stanisław Morawski <malpunek@gmail.com>
2019-10-20 18:43:46 +0200

    Style fix, delete unused imports, delete unused variables

--- run.py
+++ run.py
@@ -1,58 +1,12 @@
 import text_removal
-import ml_metrics as metrics
 import cv2
-from dataset import HistDataset
-from dataset import MaskDataset
-import distance as dist
-from utils import (
-    calc_similarities,
-    get_tops,
-    get_groundtruth,
-    normalize_hist,
-    get_mask_metrics,
-    get_mean_IoU
-)
+from utils import get_groundtruth, get_mean_IoU
 import numpy as np
 
 
-# def find_img_corresp(QS, groundTruth, masking):
-#     sims = calc_similarities(dist.canberra, DB, QS, True)
-#     tops = get_tops(sims, k)
-#     mapAtK = metrics.mapk(groundTruth, tops, k)
-#
-#     print(str(tops[0]))
-#     print(str(tops[1]))
-#     print(str(tops[2]))
-#     print("Map@k is " + str(mapAtK))
-#
-#     if masking:
-#         gt_masks = MaskDataset("datasets/qsd2_w1")
-#         mask_dataset = HistDataset("datasets/qsd2_w1", masking=True)
-#         predicted_masks = [cv2.threshold(mask_dataset.get_mask(i), 128, 1, cv2.THRESH_BINARY)[1] for i, item in enumerate(mask_dataset)]
-#
-#         mask_metrics = get_mask_metrics(predicted_masks, gt_masks, True)
-#
-#         print("Precision: " + str(mask_metrics["precision"]))
-#         print("Recall: " + str(mask_metrics["recall"]))
-#         print("F1-score: " + str(mask_metrics["f1_score"]))
-#
-#
-# groundTruth1 = get_groundtruth("datasets/qsd1_w1/gt_corresps.pkl")
-# groundTruth2 = get_groundtruth("datasets/qsd2_w1/gt_corresps.pkl")
-#
-# QS1 = [normalize_hist(qs_hist) for qs_hist in HistDataset("datasets/qsd1_w1")]
-# QS2 = [normalize_hist(qs_hist) for qs_hist in HistDataset("datasets/qsd2_w1", masking=True)]
-# DB = [normalize_hist(db_hist) for db_hist in HistDataset("datasets/DDBB", masking=False)]
-#
-# k = 10
-#
-# print("Analyzing QS1")
-# find_img_corresp(QS1, groundTruth1, False)
-#
-# print("Analyzing QS2")
-# find_img_corresp(QS2, groundTruth2, True)
-
-QS1 = [text_removal.getpoints2(im) for im in text_removal.text_remover("datasets/qsd1_w2")]
+QS1 = [
+    text_removal.getpoints2(im) for im in text_removal.text_remover("datasets/qsd1_w2")
+]
 boundingxys = [element.boundingxy for element in QS1]
 drawings = [element.drawing for element in QS1]
 
@@ -63,10 +17,3 @@ print("Mean Intersection over Union: ", mean_IoU)
 
 for im in range(len(drawings)):
     cv2.imwrite("outputs/" + str(im) + ".png", drawings[im])
-
-
-
-
-
-
-
--- text_removal.py
+++ text_removal.py
@@ -4,53 +4,51 @@ import numpy as np
 import cv2
 import utils
 import math
+
+
 class text_remover(dataset.Dataset):
-    
     def __init__(self, path):
         self.paths = sorted(glob.glob(f"{path}/*.jpg"))
         self.data = [cv2.imread(path) for path in self.paths]
 
     def __getitem__(self, idx):
-        return super().__getitem__(idx)  
+        return super().__getitem__(idx)
+
 
 def getpoints(im):
 
-    ret, thresh1 = cv2.threshold(
-    im[:,:,0], 200,255, cv2.THRESH_BINARY)
+    ret, thresh1 = cv2.threshold(im[:, :, 0], 200, 255, cv2.THRESH_BINARY)
     kernel = np.ones((4, 4), np.uint8)
     sure_bg1 = cv2.erode(thresh1, kernel, iterations=3)
 
-    ret, thresh2 = cv2.threshold(
-    im[:,:,1], 200,255, cv2.THRESH_BINARY)
- 
+    ret, thresh2 = cv2.threshold(im[:, :, 1], 200, 255, cv2.THRESH_BINARY)
+
     sure_bg2 = cv2.erode(thresh2, kernel, iterations=3)
 
-    ret, thresh3 = cv2.threshold(
-    im[:,:,2], 200,255, cv2.THRESH_BINARY)
+    ret, thresh3 = cv2.threshold(im[:, :, 2], 200, 255, cv2.THRESH_BINARY)
     sure_bg3 = cv2.erode(thresh3, kernel, iterations=3)
-    
-    final=sure_bg1-(sure_bg3-sure_bg1-sure_bg2)
+
+    final = sure_bg1 - (sure_bg3 - sure_bg1 - sure_bg2)
     return final
 
 
 def getpoints2(im):
     # print(im.shape[0])
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    imager=im[:,:,0]
-    imageg=im[:,:,1]
-    imageb=im[:,:,2]
-    output=np.equal(imager,imageg,dtype=int)
-    output2=np.equal(imager,imageb,dtype=int)
-    output1=np.equal(imageb,imageg,dtype=int)
-    output3=output&output2
-    output4=output3&output1
-    mask=255*output4
-    mask1=mask.astype(np.uint8)
+    imager = im[:, :, 0]
+    imageg = im[:, :, 1]
+    imageb = im[:, :, 2]
+    output = np.equal(imager, imageg, dtype=int)
+    output2 = np.equal(imager, imageb, dtype=int)
+    output1 = np.equal(imageb, imageg, dtype=int)
+    output3 = output & output2
+    output4 = output3 & output1
+    mask = 255 * output4
+    mask1 = mask.astype(np.uint8)
     # gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     # ret, thresh = cv2.threshold(
     # gray, 200, 255, cv2.THRESH_BINARY)
-    kernel1= np.ones((6,6),np.uint8)
-    kernel = np.ones((int((im.shape[0])/350),int((im.shape[1])/35)), np.uint8)
+    kernel = np.ones((int((im.shape[0]) / 350), int((im.shape[1]) / 35)), np.uint8)
 
     denoised = cv2.erode(mask1, kernel, iterations=5)
     denoised = cv2.erode(denoised, kernel, iterations=1)
@@ -58,35 +56,52 @@ def getpoints2(im):
     denoised = cv2.dilate(denoised, kernel, iterations=2)
 
     canny_output = cv2.Canny(denoised, 200, 255)
-    contours, hierarchy = cv2.findContours(canny_output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours_poly = [None]*len(contours)
-    boundRect = [None]*len(contours)
-    drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
-    drawing[:,:,0] = gray
-    drawing[:,:,1] = gray
-    drawing[:,:,2] = gray
-    max_area=0
-    max=0
+    contours, hierarchy = cv2.findContours(
+        canny_output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+    )
+    contours_poly = [None] * len(contours)
+    boundRect = [None] * len(contours)
+    drawing = np.zeros(
+        (canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8
+    )
+    drawing[:, :, 0] = gray
+    drawing[:, :, 1] = gray
+    drawing[:, :, 2] = gray
+    max_area = 0
+    max = 0
     for i, c in enumerate(contours):
         contours_poly[i] = cv2.approxPolyDP(c, 3, True)
         boundRect[i] = cv2.boundingRect(contours_poly[i])
-        area = boundRect[i][2]*boundRect[i][3]
+        area = boundRect[i][2] * boundRect[i][3]
         if (area > max_area) & (boundRect[i][3] < boundRect[i][2]):
-            max=boundRect[i]
-            max_area=area
+            max = boundRect[i]
+            max_area = area
         boundRect[i] = max
 
     for i in range(len(contours)):
         # cv2.drawContours(drawing, contours_poly, i, (0, 255, 0))
-        cv2.rectangle(drawing, (int(boundRect[-1][0]), int(boundRect[-1][1])), \
-          (int(boundRect[-1][0]+boundRect[-1][2]), int(boundRect[-1][1]+boundRect[-1][3])), (0, 255, 0), 2)
+        cv2.rectangle(
+            drawing,
+            (int(boundRect[-1][0]), int(boundRect[-1][1])),
+            (
+                int(boundRect[-1][0] + boundRect[-1][2]),
+                int(boundRect[-1][1] + boundRect[-1][3]),
+            ),
+            (0, 255, 0),
+            2,
+        )
 
-    boundingxy = [boundRect[-1][0], boundRect[-1][1], boundRect[-1][0] + boundRect[-1][2], boundRect[-1][1] + boundRect[-1][3]]
+    boundingxy = [
+        boundRect[-1][0],
+        boundRect[-1][1],
+        boundRect[-1][0] + boundRect[-1][2],
+        boundRect[-1][1] + boundRect[-1][3],
+    ]
 
-    ## Betos Post-Processing from here on
+    # Betos Post-Processing from here on
 
     # Otsu's thresholding
-    imx = np.copy(gray[boundingxy[1]:boundingxy[3], boundingxy[0]:boundingxy[2]])
+    imx = np.copy(gray[boundingxy[1] : boundingxy[3], boundingxy[0] : boundingxy[2]])
     ima, th2 = cv2.threshold(imx, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
     minv = np.min(th2)
@@ -109,7 +124,7 @@ def getpoints2(im):
                 for v in range(1, 15, 2):
                     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (v, v))
                     w = math.floor(v / 2)
-                    roi = th3[i - w:i + w + 1, j - w:j + w + 1]
+                    roi = th3[i - w : i + w + 1, j - w : j + w + 1]
 
                     if roi.shape == kernel.shape:
                         k = (roi * kernel).sum()
@@ -128,8 +143,26 @@ def getpoints2(im):
     delta = 0
     ddepth = cv2.CV_16S
 
-    grad_x = cv2.Sobel(gray, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
-    grad_y = cv2.Sobel(gray, ddepth, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+    grad_x = cv2.Sobel(
+        gray,
+        ddepth,
+        1,
+        0,
+        ksize=3,
+        scale=scale,
+        delta=delta,
+        borderType=cv2.BORDER_DEFAULT,
+    )
+    grad_y = cv2.Sobel(
+        gray,
+        ddepth,
+        0,
+        1,
+        ksize=3,
+        scale=scale,
+        delta=delta,
+        borderType=cv2.BORDER_DEFAULT,
+    )
 
     abs_grad_x = cv2.convertScaleAbs(grad_x)
     abs_grad_y = cv2.convertScaleAbs(grad_y)
@@ -137,7 +170,7 @@ def getpoints2(im):
     grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
     thresh = 30
 
-    grad[boundingxy[1]:boundingxy[3], boundingxy[0]:boundingxy[2]] = 0
+    grad[boundingxy[1] : boundingxy[3], boundingxy[0] : boundingxy[2]] = 0
 
     def moveboundy(maxsize, b1, b2, b3, pad, action, diff=-1):
         for i in range(b1, min(b1 + pad, maxsize), action):
@@ -169,13 +202,23 @@ def getpoints2(im):
         return b1
 
     diff1 = 0
-    boundingxy[1], diff1 = moveboundy(999999999, boundingxy[1], boundingxy[0], boundingxy[2], -50, -1, diff1)
+    boundingxy[1], diff1 = moveboundy(
+        999999999, boundingxy[1], boundingxy[0], boundingxy[2], -50, -1, diff1
+    )
     diff2 = 0
-    boundingxy[3], diff2 = moveboundy(grad.shape[0], boundingxy[3], boundingxy[0], boundingxy[2], 50, 1, diff2)
+    boundingxy[3], diff2 = moveboundy(
+        grad.shape[0], boundingxy[3], boundingxy[0], boundingxy[2], 50, 1, diff2
+    )
     boundingxy[0] = moveboundx(boundingxy[0], boundingxy[1], boundingxy[3], -50, -1)
     boundingxy[2] = moveboundx(boundingxy[2], boundingxy[1], boundingxy[3], 50, 1)
 
-    drawing = cv2.rectangle(drawing, (boundingxy[0], boundingxy[1]), (boundingxy[2], boundingxy[3]), (0, 255, 255), 2)
+    drawing = cv2.rectangle(
+        drawing,
+        (boundingxy[0], boundingxy[1]),
+        (boundingxy[2], boundingxy[3]),
+        (0, 255, 255),
+        2,
+    )
 
     if diff1 == 1:
         boundingxy[1] -= 2
@@ -201,16 +244,16 @@ def getpoints3(im):
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     imagehigh = gray > 200
     imagelow = gray < 50
-    imager = im[:,:,0]
-    imageg = im[:,:,1]
-    imageb = im[:,:,2]
-    output = np.equal(imager,imageg,dtype=int)
-    output2=np.equal(imager,imageb,dtype=int)
-    output1=np.equal(imageb,imageg,dtype=int)
-    output3=output&output2
-    output4=output3&output1
-    equalhigh=output4 & imagehigh
-    equallow=output4 & imagelow
+    imager = im[:, :, 0]
+    imageg = im[:, :, 1]
+    imageb = im[:, :, 2]
+    output = np.equal(imager, imageg, dtype=int)
+    output2 = np.equal(imager, imageb, dtype=int)
+    output1 = np.equal(imageb, imageg, dtype=int)
+    output3 = output & output2
+    output4 = output3 & output1
+    equalhigh = output4 & imagehigh
+    equallow = output4 & imagelow
 
     kernel = np.ones((int((im.shape[0]) / 350), int((im.shape[1]) / 20)), np.uint8)
     equallow = 255 * equallow
@@ -222,13 +265,14 @@ def getpoints3(im):
     equalhigh = cv2.dilate(equalhigh, kernel, iterations=6)
     equalhigh = cv2.erode(equalhigh, kernel, iterations=7)
     if equalhigh.any() > 0:
-        print('imatge blanca')
-        #TODO: Probar amb el threshold aqui, ja que saps si es blanca o negre amb un 1/30 de error.
+        print("imatge blanca")
+        # TODO: Probar amb el threshold aqui,
+        # ja que saps si es blanca o negre amb un 1/30 de error.
+        # TODO(Stas): Please comment in English
         final = 255 * imagehigh
         final = final.astype(np.uint8)
-    else :
-        print('imatge negre')
-        final=None
+    else:
+        print("imatge negre")
+        final = None
 
     return final
-
