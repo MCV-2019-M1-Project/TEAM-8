4d641934e8e5b54c4948563a0afecae50dd47389
Stanisław Morawski <malpunek@gmail.com>
2019-10-20 19:37:15 +0200

    Added pre-commit hook

---
+++ pre-commit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# to enable:
+# cp pre-commit.sh .git/hooks/pre-commit
+# chmod +x .git/hooks/pre-commit
+
+git diff --staged --name-only HEAD | grep -E '\.py$' | while read -r f; do
+    if ! pipenv run flake8 --max-line-length=88 --ignore=E203,W503 "$f"; then
+        echo "Please fix errors before commiting";
+        exit 1;
+    fi
+done
