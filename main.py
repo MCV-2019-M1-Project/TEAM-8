6c218b8af384805884cbb79f396afaf3e44651b1
Alberto L <alberto1llv@gmail.com>
2019-10-29 22:15:55 +0100

    Some refactoring.

--- main.py
+++ main.py
@@ -12,24 +12,47 @@ import text_removal
 
 import glob
 
-# TASK 1: Detect keypoints and compute descriptors
-#   Step 1: Detection
-#   Step 2: Description
+# PRE-TASK -3:  Remove Noise.
+# PRE-TASK -2:  Split images. #TODO
+# PRE-TASK -1:  Mask background. #TODO
+# PRE-TASK 0:   Mask text bounding box.
 
-# TASK 2: Find tentative matches based on similarity of local appearance and verify matches
+# TASK 1:       Detect keypoints and compute descriptors.
+#                       Step 1: Detection
+#                               Beto implemented ORB.
+#                       Step 2: Description
+#                               Beto implemented ORB.
 
-# TASK 3: Evaluate system on QSD1-W4, map@k
+# TASK 2:       Find tentative matches based on similarity of local appearance and verify matches.
+#                       Beto implemented brute force matching.
 
-# TASK 4: Evaluate best system from W3 on QSD1-W4
+# TASK 3:       Evaluate system on QSD1-W4, map@k. #TODO
+
+# TASK 4:       Evaluate best system from W3 on QSD1-W4. #TODO
+
+SHOW_IMGS = True
 
 
-def get_images(files_path, extension = "jpg"):
+def get_imgs(files_path, extension ="jpg"):
     paths = sorted(glob.glob(f"{files_path}/*." + extension))
     return [cv.imread(path) for path in paths]
 
 
-def denoise(img):
+def denoise_imgs(img):
     return cv.medianBlur(img, 3)
+
+
+def split_imgs(img):
+    return [img]
+
+
+def get_mask_background(img): # TODO
+    mask = np.full((img.shape[0], img.shape[1]), 255, dtype="uint16")
+    return mask
+
+
+def get_mask_text(img):
+    return text_removal.getpoints2(img).mask
 
 
 def detect_keypoints(detection_type, img, mask=None):
@@ -39,7 +62,7 @@ def detect_keypoints(detection_type, img, mask=None):
 
 def describe_keypoints(description_type, img, kp):
     kp, des = description_type.compute(img, kp)
-    return kp, des
+    return des
 
 
 def match_descriptions(des1, des2, method="BF"):
@@ -51,35 +74,53 @@ def match_descriptions(des1, des2, method="BF"):
         return matches
 
 
+def evaluate_pair(matches):
+    result = 0
+    for match in matches:
+        result += match.distance
+    return result
+
+
 def main():
     # Get images and denoise query set.
-    qs = get_images("datasets/qsd1_w3")
+    qs = get_imgs("datasets/qsd1_w3")
     db = cv.imread("datasets/DDBB/bbdd_00131.jpg")
-    qs_denoised = [denoise(img) for img in qs[6:7]]
+    qs_denoised = [denoise_imgs(img) for img in qs[6:7]]
 
-    # TODO: Add image separation and background removal
+    # Get mask without background and without text box of query sets.
+    qs_bck_masks = [get_mask_background(img) for img in qs_denoised]
+    qs_bb_masks = [get_mask_text(img) for img in qs_denoised]
 
-    # Get mask without text box of query sets.
-    qs_bb_infos = [text_removal.getpoints2(img) for img in qs_denoised]
-    qs_bb_masks = [bb_info.mask for bb_info in qs_bb_infos]
+    # Merge masks into a single mask
+    qs_masks = [
+        bck_mask.astype("uint16") + bb_mask.astype("uint16")
+        for bck_mask, bb_mask in zip(qs_bck_masks, qs_bb_masks)]
+
+    for qs_mask in qs_masks:
+        qs_mask[qs_mask <= 255] = 0
+        qs_mask[qs_mask > 255] = 255
+
+    qs_masks = [qs_mask.astype("uint8") for qs_mask in qs_masks]
 
     # Detect and describe keypoints in images.
     dt_type = cv.ORB_create()
 
-    qs_kp = detect_keypoints(dt_type, qs_denoised[0], qs_bb_masks[0])
-    qs_kp, qs_des = describe_keypoints(dt_type, qs_denoised[0], qs_kp)
+    qs_kp = detect_keypoints(dt_type, qs_denoised[0], qs_masks[0])
+    qs_dp = describe_keypoints(dt_type, qs_denoised[0], qs_kp)
 
     db_kp = detect_keypoints(dt_type, db)
-    db_kp, db_des = describe_keypoints(dt_type, qs_denoised[0], db_kp)
+    db_dp = describe_keypoints(dt_type, db, db_kp)
 
     # Match images
-    match = match_descriptions(qs_des, db_des)
+    matches = match_descriptions(qs_dp, db_dp)
 
-    img3 = 0
-    img3 = cv.drawMatches(qs_denoised[0], qs_kp, db, db_kp, match[:10], img3)
-    cv.imshow("matches", img3)
-    cv.waitKey()
+    if SHOW_IMGS:
+        img_matches = 0
+        img_matches = cv.drawMatches(qs_denoised[0], qs_kp, db, db_kp, matches[:10], img_matches)
+        cv.imshow("matches", img_matches)
+        cv.waitKey()
 
     print("END")
+
 
 main()
