748d5d6e30dea7f00f4e40894c8e74881432d261
Stanisław Morawski <malpunek@gmail.com>
2019-11-13 23:22:35 +0100

    Perfect angle

--- angle.py
+++ angle.py
@@ -6,18 +6,21 @@
 
 
 # %%
-from itertools import chain
 import glob
 
 import numpy as np
-from scipy.fftpack import dct
 import cv2
+from scipy.spatial.distance import euclidean
+from scipy.ndimage import uniform_filter
+from PIL import Image
 
-from segmentation import get_last
 from utils import show_img
 
+path = "datasets/qsd1_w5"
+image_paths = sorted(glob.glob(f"{path}/*.jpg"))
 
 # %%
+
 
 def get_lines(img):
     low_threshold = 50
@@ -32,87 +35,103 @@ def get_lines(img):
 
     # Run Hough on edge detected image
     # Output "lines" is an array containing endpoints of detected line segments
-    return cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
+    return cv2.HoughLinesP(
+        edges, rho, theta, threshold, np.array([]), min_line_length, max_line_gap
+    )
 
 
-def draw_lines(img, lines)
+def draw_lines(img, lines):
     line_image = np.zeros_like(img)  # creating a blank to draw lines on
 
     for line in lines:
-        for x1,y1,x2,y2 in line:
-            cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
+        for x1, y1, x2, y2 in line:
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 5)
 
-    lines_edges = cv2.addWeighted(img, 0.8, line_image, 1, 0)
+    # lines_edges = cv2.addWeighted(img, 0.8, line_image, 1, 0)
+    # cv2.addWeighted(img, 0.8, line_image, 1, 0)
 
     return line_image
 
 
+def draw_lines_on(img, lines):
+    line_img = draw_lines(img, lines)
+    return cv2.addWeighted(img, 0.8, line_img, 1, 0)
+
+
 # %%
 
-path = "datasets/qsd1_w5"
-image_paths = sorted(glob.glob(f"{path}/*.jpg"))
-
-
-def one(image_path):
-    img = get_last(image_path)
-    show_img(img)
-    kernel_size = 7
-    blur_gray = cv2.medianBlur(img,kernel_size)
-    show_img(img)
-    line_img = get_lines(blur_gray)
-    show_img(line_img, title=image_path)
-
-
-def two(image_path):
-    img = cv2.imread(image_path)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+def get_all_lines(cvimg, iters=3):
+    gray = cv2.cvtColor(cvimg, cv2.COLOR_BGR2GRAY)
 
     kernel_size = 5
-    
-    blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
-    line_img = get_lines(blur_gray)
-    show_img(line_img, title=image_path)
+    line_img = cv2.GaussianBlur(gray, (kernel_size, kernel_size), 0)
 
-
-def three(image_path):
-
-    img = cv2.imread(image_path)
-    imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(imgray, 127, 255, 0)
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_TC89_KCOS)
-    img = np.zeros(img.shape, np.uint8)
-    print(f"SHAPE {img.shape}")
-    img = cv2.drawContours(img, contours, -1, (255,255,255), 3)
-    show_img(img)
-    line_img = get_lines(img)
-    show_img(line_img, title=image_path)
-
-
-def four(image_path, iters=3):
-    img = cv2.imread(image_path)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-    show_img(gray)
-
-    kernel_size = 5
-    line_img = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
-
-    kernel_size = 40    
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(kernel_size,kernel_size))
+    kernel_size = 40
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel_size, kernel_size))
     for i in range(iters):
-        line_img = get_lines(line_img)
-        # show_img(line_img, title=image_path)
+        lines = get_lines(line_img)
         if i != iters - 1:
+            line_img = draw_lines(line_img, lines)
             line_img = cv2.dilate(line_img, kernel)
-            # show_img(line_img, title=image_path)
-    show_img(line_img, title=image_path)
+
+    line_img = draw_lines(line_img, lines)
+    # show_img(line_img, title=image_path)
+    return lines
 
 
+def get_angle(line):
+    x1, y1, x2, y2 = line
+    angle = np.rad2deg(np.arctan2(y2 - y1, x2 - x1))
+    weight = euclidean((x1, y1), (x2, y2))
+    angle = angle % 90
+    return int(angle), weight
 
-for path in image_paths:
-    print(path)
-    # one(path)
-    # two(path)
-    # three(path)
-    four(path)
 
+def get_horiz_angle(lines):
+    weighted_angles = [get_angle(line[0]) for line in lines]
+    possible = np.zeros(90)
+    for a, w in weighted_angles:
+        possible[a] += w
+    possible = uniform_filter(possible, size=2)
+    final_angle = np.argmax(possible)
+
+    return final_angle
+
+
+def draw_horizontal_lines(cvimg, lines, horiz_angle):
+    def is_close_to_horiz(line):
+        return abs(get_angle(line[0])[0] - horiz_angle) < 2
+
+    lines = filter(is_close_to_horiz, lines)
+
+    cvimg = draw_lines_on(cvimg, lines)
+    return cvimg
+
+
+def get_rotated(path, horiz_angle):
+    if horiz_angle > 45:
+        rotation = -abs(90 - horiz_angle)
+    else:
+        rotation = horiz_angle
+
+    img = Image.open(path)
+    r, g, b = img.split()
+    img = Image.merge("RGB", (b, g, r))
+    img = img.rotate(angle=rotation, expand=False)
+    return np.array(img)
+
+
+def read_horizontal_image(path):
+    img = cv2.imread(path)
+    lines = get_all_lines(img)
+    angle = get_horiz_angle(lines)
+    return get_rotated(path, angle)
+
+
+if __name__ == '__main__':
+    for path in image_paths:
+        img = cv2.imread(path)
+        lines = get_all_lines(img)
+        angle = get_horiz_angle(lines)
+        show_img(draw_horizontal_lines(img, lines, angle))
+        show_img(get_rotated(path, angle))
